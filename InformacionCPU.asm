c3ea3dd3c10646dcc0fab1fd67b6dfcb7432f54e
aarroyo23r <aarroyo23r@gmail.com>
2017-08-18 13:02:40 -0600

    Muestra el stepping ID,modelo,familia,tipo,modelo extendido

--- InformacionCPU.asm
+++ InformacionCPU.asm
@@ -1,15 +1,84 @@
-;--------------------Segmento de datos--------------------
+
+;------Segmento de macros--------------------
+
+;-------------------------  MACRO #1  ----------------------------------
+;Programa Elaborado por Simon Whitehead, 2015
+;Reestructurado en una serie de macros para ser utilizado en este programa
+; ---------------------
+; Convierte numeros enteros a sus caracteres ASCII y los imprime
+
+
+%macro printt 2
+    mov rax,4
+    mov rbx,1
+    mov rcx,%1 ;rdi
+    mov rdx,%2 ;rsi
+    int 0x80
+%endmacro
+
+%macro itoa 1
+    push rbp
+    mov rbp,rsp
+    sub rsp,4		; allocate 4 bytes for our local string length counter
+
+    mov rax,rdi		; Move the passed in argument to rax
+    lea rdi,[%1+10]	; load the end address of the buffer (past the very end)
+    mov rcx,10		; divisor
+    mov [rbp-4],dword 0	; rbp-4 will contain 4 bytes representing the length of the string - start at zero
+%endmacro
+
+%macro printInt 2
+    mov rdi,%1	; Move the number (123456789) into rax
+    itoa	%2	; call the function
+
+		call .divloop
+
+		; Write the string returned in rax out to stdout
+		mov rdi,rax		; The string pointer is returned in rax - move it to rdi for the function call
+		mov rsi,rcx
+		printt rdi,rsi
+		printt 10,1
+
+
+%endmacro
+
+;------------------------- FIN DE MACRO --------------------------------
+
+
+%include "Macros.mac"  ;Inclusión de los demas macros
+
+
+;------Segmento de datos--------------------
+
 ;Aqui se declaran las constantes de uso frecuente en el programa
-section .data 
-
-        ;Textos auxiliares
-        salida: db 'Cerrando programa ', 0xa	; Instrucciones para el usuario
-        tam_salida: equ $-salida					; Longitud
-
+section .data
 
         ;Información  a Imprimir
-        id db "id del Vendedor = 'XXXXXXXXXXXX'",0xA
-        modelo db "Modelo = 'xxxxxxxxxxxx'",0xA
+        id db "id del Vendedor = XXXXXXXXXXXX",0xA
+        tam_id: equ $-id
+
+				SteppingID db 'Stepping ID = '
+        tam_SteppingID: equ $-SteppingID
+
+        modelo db 'Modelo = '
+        tam_modelo: equ $-modelo
+
+				familia db 'Familia = '
+        tam_familia: equ $-familia
+
+				tipo db 'Tipo de Procesador= '
+        tam_tipo: equ $-tipo
+
+				ModeloExt db 'Modelo Extendido = '
+        tam_ModeloExt: equ $-ModeloExt
+
+				SLinea db 0xA
+
+
+section .bss
+				;Para el Macro printInt
+				 numbuf resb 10		; A buffer to store our string of numbers in
+
 ;###################################################
 
 ;--------------------Segmento de codigo--------------------
@@ -25,55 +94,102 @@ _start:
         mov eax,0    ;Pone eax en 0
         cpuid        ;Toma la información
         mov edi,id  ;Copia el string id para modificarlo con la nueva información
-        mov [edi+19],ebx;Copia los primeros 4 bytes en las posiciones de las x
-        mov [edi+23],edx;Copia los siguientes bytes en las posiciones de las x
-        mov [edi+27],ecx;Copia los ultimos bytes en las posiciones de las x
-
-        ;Imprime el texto ya modificado
-        mov eax,4
-        mov ebx,1
-        mov ecx,id
-        mov edx,41 ;Tamaño
-        int 0x80
+        mov [edi+18],ebx;Copia los primeros 4 bytes en las posiciones de las x
+        mov [edi+22],edx;Copia los siguientes bytes en las posiciones de las x
+        mov [edi+26],ecx;Copia los ultimos bytes en las posiciones de las x
+        print id,tam_id ;Imprime el id
 
 
 ;###############################################################################
         ;Version Information: Type, Family, Model, and Stepping ID
+
         mov eax,1   ;Pone eax en 1
         cpuid       ;Toma la información
+				mov r15,rax ;guarda la informacion de rax en otro registro para reutilizarla
+
+				;_______________________________________________________________________
+        ;Stepping ID
+
+        ;Imprime el texto Stepping ID =
+        print SteppingID,tam_SteppingID
+
+        ;Obteniendo el modelo
+        ;El modelo esta dado en eax[7:4]
+				mov rdx,r15   ;copia el contenido de rax
+        and rdx,0xf  ;deja solamente la informacion del modelo
+        printInt rdx,numbuf ;Imprime el Stepping ID
+				print SLinea,1   ;Salto de linea
+
+        ;_______________________________________________________________________
         ;Modelo
 
-        ;El modelo esta dato en eax[7:4]
-        mov r10d,eax   ;copia el contenido de eax
-        and r10d,0x10  ;deja solamente la informacion del modelo
+        ;Imprime el texto Modelo =
+        print modelo,tam_modelo
 
-        ;Comparaciones para determinar el texto del modelo
+        ;Obteniendo el modelo
+        ;El modelo esta dado en eax[7:4]
+				mov rdx,r15   ;copia el contenido de rax
+        and rdx,0xf0  ;deja solamente la informacion del modelo
+				shr rdx,4  		;desplaza el modelo a la posicion correcta
+        printInt rdx,numbuf ;Imprime el modelo
+				print SLinea,1   ;Salto de linea
 
-        cmp r10d,0x90
-        jne .fin
+				;_______________________________________________________________________
+				;Familia
 
-        ;mov edi,modelo;Copia el texto a modificar
-        ;mov [edi+10],0
+				;Imprime el texto Modelo =
+				print familia,tam_familia
+        ;Obteniendo la familia
+        mov rdx,r15   ;copia el contenido de rax
+        and rdx,0xf00  ;deja solamente la informacion la familia
+				shr rdx,8 		;desplaza la familia a la posicion correcta
+        printInt rdx,numbuf
+				print SLinea,1   ;Salto de linea
 
-        ;Imprime el texto
-        mov eax,4
-        mov ebx,1
-        mov ecx,modelo
-        mov edx,41
-        int 0x80
+				;_______________________________________________________________________
+				;Tipo
 
+				;Imprime el texto Tipo =
+				print tipo,tam_tipo
+        ;Obteniendo la familia
+        mov rdx,r15   ;copia el contenido de rax
+        and rdx,0xf000
+				shr rdx,12
+        printInt rdx,numbuf
+				print SLinea,1   ;Salto de linea
 
-        ;Fin
-        .fin:
+				;_______________________________________________________________________
+				;Modelo Extendido
 
-        ;Mensaje de fin
+				;Imprime el texto Modelo Extendido =
+				print ModeloExt,tam_ModeloExt
+				;Obteniendo la familia
+				mov rdx,r15   ;copia el contenido de rax
+				and rdx,0xf0000
+				shr rdx,16
+				printInt rdx,numbuf
+				print SLinea,1   ;Salto de linea
 
-      	mov rax,1							;rax = "sys_write"
-      	mov rdi,1							;rdi = 1 (standard output = pantalla)
-      	mov rsi,salida					;rsi = mensaje a imprimir
-      	mov rdx,tam_salida						;rdx=solo se imprime 1 byte
-      	syscall								;Llamar al sistema
-
+				;Fin del programa
+				;Se liberan los recursos
         mov eax,1
         xor ebx,ebx
         int 0x80
+
+
+;Loop de division necesario para el macro printInt
+		 .divloop:
+						xor rdx,rdx		; Zero out rdx (where our remainder goes after idiv)
+						idiv rcx		; divide rax (the number) by 10 (the remainder is placed in rdx)
+						add rdx,0x30	; add 0x30 to the remainder so we get the correct ASCII value
+						dec rdi		; move the pointer backwards in the buffer
+						mov byte [rdi],dl	; move the character into the buffer
+						inc dword [rbp-4]	; increase the length
+
+						cmp rax,0		; was the result zero?
+						jnz .divloop	; no it wasn't, keep looping
+
+						mov rax,rdi		; rdi now points to the beginning of the string - move it into rax
+						mov rcx,[rbp-4]	; rbp-4 contains the length - move it into rcx
+
+						ret
