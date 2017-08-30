5a42f7c3bb2ff4e2783b90b48dec206ce8ffde43
John-JT <juneths96@gmail.com>
2017-08-30 08:48:39 -0600

    Update InfoSO1.asm
    Se comentó el código para un mejor entendimiento del mismo

--- InfoSO1.asm
+++ InfoSO1.asm
@@ -41,14 +41,15 @@ section .data
   cons_kernelversion: db 'Kernel Version: '
   cons_tam_kernelversion: equ $-cons_kernelversion
   cons_machine: db 'Machine: '
-  cons_tam_machine: equ $-cons_machine
+  cons_tam_machine: equ $-cons_machine        ; Definición de los strings y sus tamaños para su impresión en consola
 
 
 InfoSO:
 
   mov rax, SYS_UNAME
   mov rdi, uname_res
-  syscall
+  syscall            ;Básicamente esta es la parte donde se busca la información del sistema operativo proveniente del comando en consola 
+  		     ; uname
 
   mov rdi, 1
   cmp rax, 0
@@ -61,7 +62,7 @@ InfoSO:
 
 print_all_utsname:
   xor rcx, rcx
-  xor rbx, rbx
+  xor rbx, rbx 
   mov cl, 5
 
   mov rdi, STDOUT
@@ -69,7 +70,10 @@ print_all_utsname:
   L1:
     push rcx
 
-    cmp rbx, 0
+			;Estas comparaciones se dan cada 65 bits porque la información de cada uno de los apartados del comando "uname"
+			;están contenidas en arreglos de ese tamaño. Por lo tanto, se hace la comparación se imprime el string respectivo
+			;y contiguamente se imprime la información que corresponde a ese apartado del sistema operativo.
+    cmp rbx, 0		
     je kernel
 
     cmp rbx, 65
@@ -85,12 +89,17 @@ print_all_utsname:
     je machine
 
    .CNT:
+   				;Esta parte se encarga de tanto imprimir la información del sistema operativo en consola, además suma 
+				;65 bits cada vez que ya se imprimió la información de la posición en memoria previa para continuar 
+				;con el resto de apartados del sistema operativo.				
     mov rax, SYS_WRITE
     mov rdx, UTSNAME_SIZE
     lea rsi, [uname_res + rbx]
     add bx, UTSNAME_SIZE
     syscall
 
+				;Cada vez que se imprime uno de los apartados de la inforamción del sistema operativo se procede a realizar
+				;un "enter" para imprimir lo siguiente en la próxima fila.
     mov rax, SYS_WRITE
     mov rsi, break_line
     mov rdx, 1
@@ -110,27 +119,29 @@ print_all_utsname:
 exit:
   ret
 
+			; De esta línea en adelante se llaman las funciones respectivas para imprimir los strings que acompañan
+			;la información relativa al sistema operativo: Kernel, Hostname, Kernelrelease, Kernelversion & Machine
 kernel:
 
   impr_texto cons_kernel, cons_tam_kernel
-  jmp L1.CNT
+  jmp L1.CNT		;Una vez impreso el string respectivo se vuelve al ciclo para imprimir la información respectiva del sistema operativo
 
 hostname:
 
   impr_texto cons_hostname, cons_tam_hostname
-  jmp L1.CNT
+  jmp L1.CNT		;Una vez impreso el string respectivo se vuelve al ciclo para imprimir la información respectiva del sistema operativo
 
 kernelrelease:
 
   impr_texto cons_kernelrelease, cons_tam_kernelrelease
-  jmp L1.CNT
+  jmp L1.CNT		;Una vez impreso el string respectivo se vuelve al ciclo para imprimir la información respectiva del sistema operativo
 
 kernelversion:
 
   impr_texto cons_kernelversion, cons_tam_kernelversion
-  jmp L1.CNT
+  jmp L1.CNT		;Una vez impreso el string respectivo se vuelve al ciclo para imprimir la información respectiva del sistema operativo
 
 machine:
 
   impr_texto cons_machine, cons_tam_machine
-  jmp L1.CNT
+  jmp L1.CNT		;Una vez impreso el string respectivo se vuelve al ciclo para imprimir la información respectiva del sistema operativo
